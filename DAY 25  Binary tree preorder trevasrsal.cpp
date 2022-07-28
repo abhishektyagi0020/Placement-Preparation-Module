7534a40217645b39bed6730810e83663c575d523
ABHISHEK TYAGI <94224779+abhishektyagi0020@users.noreply.github.com>
2022-07-28 19:26:16 +0530

    Update DAY 25  Binary tree preorder trevasrsal.cpp
    Signed-off-by: ABHISHEK TYAGI  <94224779+abhishektyagi0020@users.noreply.github.com>

--- DAY 25  Binary tree preorder trevasrsal.cpp
+++ DAY 25  Binary tree preorder trevasrsal.cpp
@@ -1,6 +1,43 @@
 // Problem 1 binary tree perorder trevarsal .cpp
 
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *l;
+ *     TreeNode *r;
+ *     TreeNode() : val(0), l(nullptr), r(nullptr) {}
+ *     TreeNode(int x) : val(x), l(nullptr), r(nullptr) {}
+ *     TreeNode(int x, TreeNode *l, TreeNode *r) : val(x), l(l), r(r) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        stack<TreeNode*>st;// stack imput 
+        TreeNode* node=root;
+        vector<int> inorder; // vector 
+ 
+        while(true)
+        {
+if(node!=NULL)
+{
+st.push(node);
+node=node->left;
+}
+        else
+        {
+        if(st.empty() == true)
+            break;
+            node=st.top();
+            st.pop();
+            inorder.push_back(node->val);
+            node=node->right;
+            
+        }}
+return inorder;
+    }
+};
 //problem 2Binary Tree Inorder Traversal.cpp
 
 
