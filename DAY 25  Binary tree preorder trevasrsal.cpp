54fbd9b939bc358959d788ccb6c6a87a7c6c0eb0
ABHISHEK TYAGI <94224779+abhishektyagi0020@users.noreply.github.com>
2022-07-29 00:18:43 +0530

    Update DAY 25  Binary tree preorder trevasrsal.cpp
    Signed-off-by: ABHISHEK TYAGI  <94224779+abhishektyagi0020@users.noreply.github.com>

--- DAY 25  Binary tree preorder trevasrsal.cpp
+++ DAY 25  Binary tree preorder trevasrsal.cpp
@@ -69,4 +69,30 @@ if(root->left!=NULL)
 
 //problem 3 Binary Tree postorder Traversal
 
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+       vector<int>postorder;
+        if(root ==NULL)
+            return postorder;
+        stack<TreeNode* >s1,s2;
+        s1.push(root);
+        while(!s1.empty())
+        {
+    root=s1.top();
+            s1.pop();
+            s2.push(root);
+            if(root->left!=NULL)
+                s1.push(root->left);
+            if(root->right!=NULL)
+                s1.push(root->right);
+        }
+        while(!s2.empty())
+        {
+            postorder.push_back(s2.top()->val);
+            s2.pop();
+        }
+        return postorder;
+        }
+};
 
