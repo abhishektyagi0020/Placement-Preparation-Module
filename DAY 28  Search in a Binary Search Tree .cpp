05da72f3d988acbaa41855effe61da592d571fd3
ABHISHEK TYAGI <94224779+abhishektyagi0020@users.noreply.github.com>
2022-07-31 23:37:24 -0700

    Update DAY 28  Search in a Binary Search Tree .cpp
    Signed-off-by: ABHISHEK TYAGI  <94224779+abhishektyagi0020@users.noreply.github.com>

--- DAY 28  Search in a Binary Search Tree .cpp
+++ DAY 28  Search in a Binary Search Tree .cpp
@@ -1,6 +1,20 @@
  Search in a Binary Search Tree.cpp
  // leetcode problem 1 .cpp
  
+ class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root==NULL)
+            return NULL;
+        if(root->val==val)
+            return root;
+        TreeNode *left_side=searchBST(root->left,val);
+        if(left_side!=NULL)
+            return left_side;
+        TreeNode *right_side=searchBST(root->right,val);
+        return right_side;
+    }
+};
  
  
  
